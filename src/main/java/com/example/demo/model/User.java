2dcda8feac561103bd2e8cf6d7386ea9353e7b1c
datquocngo <shaco1409199@gmail.com>
2020-06-15 13:37:45 +0700

    a

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,11 +1,9 @@
 package com.example.demo.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "tbl_User")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
